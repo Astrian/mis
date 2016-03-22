d8593528dc29e7ef8df3de47dcb66261b63f1f40
Astrian Zheng <mingriqd@gmail.com>
2016-03-22 13:43:34 +0800

    清理调试数据

--- src/javaapplication1/login.java
+++ src/javaapplication1/login.java
@@ -54,8 +54,6 @@ public class login extends javax.swing.JFrame {
         jLabel2.setFont(new java.awt.Font("微软雅黑", 0, 12)); // NOI18N
         jLabel2.setText("使用管理员账户登录图书借阅系统");
 
-        Username.setText("Astrian");
-
         jLabel3.setFont(new java.awt.Font("微软雅黑", 0, 12)); // NOI18N
         jLabel3.setText("用户名");
 
@@ -74,7 +72,6 @@ public class login extends javax.swing.JFrame {
             }
         });
 
-        Pwd.setText("abc123");
         Pwd.setToolTipText("");
         Pwd.setCursor(new java.awt.Cursor(java.awt.Cursor.TEXT_CURSOR));
         Pwd.addActionListener(new java.awt.event.ActionListener() {
